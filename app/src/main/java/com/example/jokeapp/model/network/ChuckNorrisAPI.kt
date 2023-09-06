a50ed1d214ecf57a9576b0f567194c580f713550
Mateus da Silva <mso3@cin.ufpe.br>
2023-09-06 09:30:14 -0300

    add news endpoint for joke

--- app/src/main/java/com/example/jokeapp/model/network/ChuckNorrisAPI.kt
+++ app/src/main/java/com/example/jokeapp/model/network/ChuckNorrisAPI.kt
@@ -1,5 +1,6 @@
 package com.example.jokeapp.model.network
 
+import com.example.jokeapp.model.Joke
 import com.example.jokeapp.model.network.HTTPClient
 import com.example.jokeapp.util.Constants.API_KEY
 import retrofit2.Call
@@ -9,5 +10,11 @@ import retrofit2.http.Query
 interface ChuckNorrisAPI {
     @GET("jokes/categories")
     fun findAllCategories(@Query("apiKey")apiKey: String = API_KEY): Call<List<String>>
+
+    @GET("jokes/random")
+    fun findJoke(@Query("category")categoryName:String, @Query("apiKey")apiKey: String = API_KEY): Call<Joke>
+
+    @GET("jokes/random")
+    fun findJokeDay(@Query("apiKey")apiKey: String = API_KEY): Call<Joke>
 
 }
