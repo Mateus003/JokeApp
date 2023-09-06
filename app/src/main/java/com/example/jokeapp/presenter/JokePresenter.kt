4591a4bc079b563fc8f289c9b7bc5e8827ebea0f
Mateus da Silva <mso3@cin.ufpe.br>
2023-09-06 09:35:50 -0300

    config JokePresenter

--- app/src/main/java/com/example/jokeapp/presenter/JokePresenter.kt
+++ app/src/main/java/com/example/jokeapp/presenter/JokePresenter.kt
@@ -1,4 +1,28 @@
 package com.example.jokeapp.presenter
 
-class JokePresenter {
+import com.example.jokeapp.model.Category
+import com.example.jokeapp.model.Joke
+import com.example.jokeapp.model.callback.JokeCallback
+import com.example.jokeapp.model.data.JokeRemoteDataSource
+import com.example.jokeapp.view.fragments.JokeFragment
+
+class JokePresenter(private val view:JokeFragment,
+            private val dataSource:JokeRemoteDataSource = JokeRemoteDataSource()):JokeCallback {
+    fun findBy(categoryName:String){
+        dataSource.findJoke(categoryName, this)
+    }
+
+    override fun onSuccess(response: Joke) {
+        view.showJoke(response)
+    }
+
+    override fun onError(response: String) {
+        view.showFailure(response)
+    }
+
+    override fun onComplete() {
+        view.hideProgressBar()
+    }
+
+
 }
