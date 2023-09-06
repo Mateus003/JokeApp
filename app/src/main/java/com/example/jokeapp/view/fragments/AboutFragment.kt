fe1ff33a8cba3106fd42e9b56285400849444cd5
Mateus da Silva <mso3@cin.ufpe.br>
2023-09-06 09:26:24 -0300

    config AboutFragment

--- app/src/main/java/com/example/jokeapp/view/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/jokeapp/view/fragments/AboutFragment.kt
@@ -1,18 +1,29 @@
 package com.example.jokeapp.view.fragments
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.jokeapp.BuildConfig
 import com.example.jokeapp.R
+import com.example.jokeapp.databinding.FragmentAboutBinding
 
 class AboutFragment: Fragment() {
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.txtVersion.text = getString(R.string.version_name, BuildConfig.VERSION_NAME)
     }
 }
