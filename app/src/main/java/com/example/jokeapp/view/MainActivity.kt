0a3180664c15741a305c5b71b89ad429a8e6ebae
Mateus da Silva <mso3@cin.ufpe.br>
2023-09-06 09:32:41 -0300

    moved MainActivity for folder View

--- app/src/main/java/com/example/jokeapp/view/MainActivity.kt
+++ app/src/main/java/com/example/jokeapp/view/MainActivity.kt
@@ -1,18 +1,16 @@
-package com.example.jokeapp
+package com.example.jokeapp.view
 
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.jokeapp.R
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
